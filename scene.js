6cda795bacf0d43c48f7519e2dc0a2cfa728c575
Bram Pieters <s1110012@student.windesheim.nl>
2019-02-08 15:20:37 +0100

    init

--- scene.js
+++ scene.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Create scene
 var scene = new THREE.Scene();
 
@@ -26,33 +25,4 @@ var render = function () {
     renderer.render(scene, camera);
 };
 
-=======
-// Create scene
-var scene = new THREE.Scene();
-
-// Create camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Create renderer
-var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Add components
-
-light.setLight(scene);
-
-// ^^^^
-
-var clock = new THREE.Clock();
-
-var render = function () {
-    requestAnimationFrame(render);
-    var delta = clock.getDelta();
-
-    //component.render();
-    renderer.render(scene, camera);
-};
-
->>>>>>> init
 render();
