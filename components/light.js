a0800e5fbcc60fbe9306911f9dcdf6d742fa3375
Bram Pieters <s1110012@student.windesheim.nl>
2019-02-08 16:14:56 +0100

    Add light component

--- components/light.js
+++ components/light.js
@@ -1,7 +1,31 @@
 const light = (function () {
-    let lights = 1;
+    var helpers = [];
 
-    function setLight(scene) {
-        scene.add(lights);
+    function keyLight(makeHelper) {
+        const l = new THREE.DirectionalLight(0xffffff, 0.5);
+        l.position.set(1, 2, 1);
+
+        if (makeHelper)
+            helpers.push(new THREE.DirectionalLightHelper(l, 1, 0xeeeeee));
+
+        return l;
     }
+
+    function setLights(scene) {
+        // Add each wanted light
+        scene.add(keyLight(true));
+
+        // Add all helpers to scene
+        helpers.forEach(helper => {
+            scene.add(helper);
+        });
+    }
+
+    function render() {
+
+    }
+    return {
+        setLights: setLights,
+        render: render
+    };
 })()
--- scene.js
+++ scene.js
@@ -3,17 +3,31 @@ var scene = new THREE.Scene();
 
 // Create camera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 1, 5);
 
 // Create renderer
 var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Create simple geometry
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshStandardMaterial();
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+var ambLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambLight);
 // Add components
 
-light.setLight(scene);
+light.setLights(scene);
 
 // ^^^^
+
+controls = new THREE.OrbitControls(camera);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2;
+controls.noKeys = true;
 
 var clock = new THREE.Clock();
 
@@ -21,7 +35,7 @@ var render = function () {
     requestAnimationFrame(render);
     var delta = clock.getDelta();
 
-    //component.render();
+    light.render();
     renderer.render(scene, camera);
 };
 
