95bcbfd722a92142465a49f5a4139380fe321376
BramblePie <43555369+BramblePie@users.noreply.github.com>
2019-02-08 15:32:28 +0100

    Add files via upload

---
+++ scene.js
@@ -0,0 +1,28 @@
+// Create scene
+var scene = new THREE.Scene();
+
+// Create camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Create renderer
+var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add components
+
+light.setLight(scene);
+
+// ^^^^
+
+var clock = new THREE.Clock();
+
+var render = function () {
+    requestAnimationFrame(render);
+    var delta = clock.getDelta();
+
+    //component.render();
+    renderer.render(scene, camera);
+};
+
+render();
